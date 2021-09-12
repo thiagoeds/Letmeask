79583b56181195e7944c3f9843d16666a8f238a0
thiagoeds <79373952+thiagoeds@users.noreply.github.com>
2021-09-12 19:42:05 -0300

    Revert "Final da aula 5"
    This reverts commit 8073c029d7d62e3b91ac437be9b309dc7b1d7ece.

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react';
-import cx from 'classnames';
 
 import '../styles/question.scss';
 
@@ -11,25 +10,15 @@ type QuestionProps = {
     };
 
     children?: ReactNode;
-    isAnswered?: boolean;
-    isHighlighted?: boolean;
 }
 
 export function Question ({
     content,
     author,
-    isAnswered = false,
-    isHighlighted = false,
     children,
 }: QuestionProps) {
     return (
-        <div 
-            className={cx(
-                'question',
-                { answered: isAnswered },
-                { highlighted: isHighlighted && !isAnswered },
-            )}
-        >
+        <div className="question">
             <p>{content}</p>
             <footer>
                 <div className="user-info">
